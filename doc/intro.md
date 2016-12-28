baefe20a3856de15031d39aac823752fc1a88622
sarudak <sarudak@gmail.com>
2016-12-28 12:37:32 -0800

    These tests suck

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to random-test-fails
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/random_test_fails/core_test.clj
@@ -0,0 +1,20 @@
+(ns random-test-fails.core-test
+  (:require [clojure.test :refer :all]
+            [random-test-fails.core :refer :all]))
+
+(deftest always-successful
+  (testing "This always succeeds"
+    (is (= 1 1))))
+
+
+(deftest almost-always-successful
+  (testing "This almost always succeeds"
+    (is (> (rand-int 100) 5))))
+
+(deftest usually-successful
+  (testing "This usually succeeds"
+    (is (> (rand-int 100) 15))))
+
+(deftest rarely-successful
+  (testing "This almost always succeeds"
+    (is (> (rand-int 100) 40))))
